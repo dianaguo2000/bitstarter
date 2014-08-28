543d326028918365a9ba8093ca8664e66ebd5e03
John Smith <example@stanford.edu>
2014-08-28 03:12:26 +0000

    updated index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+#!/usr/bin/env node
+var fs = require('fs');
 var express = require('express')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+var buf=fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!')
+  response.send(buf.toString);
 })
 
 app.listen(app.get('port'), function() {
